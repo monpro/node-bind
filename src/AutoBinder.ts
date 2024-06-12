d0875851ffd7925dd6b245ed88be7f40cd47a890
monpro <monproshen@gmail.com>
2024-06-12 20:15:07 +1000

    feat: refactor the AutoBinder to get rid of generics during init

--- src/AutoBinder.ts
+++ src/AutoBinder.ts
@@ -1,52 +1,62 @@
 type Callback<T> = (newValue: T) => void
 
+export default class AutoBinder {
+    private bindings: Map<string, Map<unknown, Callback<unknown>[]>> = new Map();
 
-export default class AutoBinder<O> {
-    private bindings: Map<keyof O, Callback<O[keyof O]>[]> = new Map()
+    bind<O, K extends keyof O>(object: O, property: K, callback: Callback<O[K]>) {
+        const objectBindings = this.bindings.get(property as string) || new Map();
 
-    bind<K extends keyof O>(object: O, property: K, callback: Callback<O[K]>) {
-        if (!this.bindings.has(property)) {
-            this.bindings.set(property, [])
-            this.watchProperty(object, property)
+        if (!objectBindings.has(object)) {
+            objectBindings.set(object, []);
+            this.watchProperty(object, property);
         }
-        this.bindings.get(property)!.push(callback as Callback<O[keyof O]>);
+
+        objectBindings.get(object)!.push(callback as Callback<unknown>);
+        this.bindings.set(property as string, objectBindings);
     }
 
-    bindMultiple<K extends keyof O>(object: O, properties: K[], callback: Callback<O[K]>) {
+    bindMultiple<O, K extends keyof O>(object: O, properties: K[], callback: Callback<O[K]>) {
         properties.forEach(property => this.bind(object,property, callback))
     }
 
-    unbind<K extends keyof O>(object: O, property: K): void {
-        if (this.bindings.has(property)) {
-            this.bindings.set(property, [])
+
+    unbind<O, K extends keyof O>(object: O, property: K): void {
+        const objectBindings = this.bindings.get(property as string);
+        if (objectBindings && objectBindings.has(object)) {
+            objectBindings.delete(object);
         }
     }
 
-    unbindCallback<K extends keyof O>(object: O, property: K, callback: Callback<O[K]>): void {
-        const callbacks = this.bindings.get(property);
-        if (callbacks) {
-            this.bindings.set(property, callbacks.filter(cb => cb !== callback))
+    unbindCallback<O, K extends keyof O>(object: O, property: K, callback: Callback<O[K]>): void {
+        const objectBindings = this.bindings.get(property as string);
+        if (objectBindings) {
+            const callbacks = objectBindings.get(object);
+            if (callbacks) {
+                objectBindings.set(object, callbacks.filter(cb => cb !== callback));
+            }
         }
     }
 
-
-    private watchProperty<K extends keyof O>(object: O, property: K ) {
-        let currentValue: O[K] = object[property]
+    private watchProperty<O, K extends keyof O>(object: O, property: K) {
+        let currentValue: O[K] = object[property];
         Object.defineProperty(object, property, {
             get: () => currentValue,
             set: (newValue: O[K]) => {
-                currentValue = newValue
-                this.notify(property, newValue)
+                currentValue = newValue;
+                this.notify(property, object, newValue);
             },
             configurable: true,
             enumerable: true,
-        })
+        });
     }
 
-    private notify<K extends keyof O>(property: K, newValue: O[K]) {
-        const subscriptions = this.bindings.get(property)
-        if (subscriptions) {
-            subscriptions.forEach(callback => callback(newValue))
+    private notify<O, K extends keyof O>(property: K, object: O, newValue: O[K]) {
+        const objectBindings = this.bindings.get(property as string);
+        if (objectBindings) {
+            const callbacks = objectBindings.get(object);
+            if (callbacks) {
+                callbacks.forEach(callback => callback(newValue));
+            }
         }
     }
 }
