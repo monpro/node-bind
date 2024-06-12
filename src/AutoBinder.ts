ae1e55131b1e5ace20d17bf30359b7f85629c79a
monpro <monproshen@gmail.com>
2024-06-12 19:58:27 +1000

    feat: add unbind and unbindCallback functions

--- src/AutoBinder.ts
+++ src/AutoBinder.ts
@@ -12,6 +12,19 @@ export default class AutoBinder<O> {
         this.bindings.get(property)!.push(callback as Callback<O[keyof O]>);
     }
 
+    unbind<K extends keyof O>(object: O, property: K): void {
+        if (this.bindings.has(property)) {
+            this.bindings.set(property, [])
+        }
+    }
+
+    unbindCallback<K extends keyof O>(object: O, property: K, callback: Callback<O[K]>): void {
+        const callbacks = this.bindings.get(property);
+        if (callbacks) {
+            this.bindings.set(property, callbacks.filter(cb => cb !== callback))
+        }
+    }
+
 
     private watchProperty<K extends keyof O>(object: O, property: K ) {
         let currentValue: O[K] = object[property]
